693c71eb94b02c0e87c41d6531c8572fb3134be8
Onyeami <134443934+Onyeami@users.noreply.github.com>
2026-04-02 22:11:53 +0100

    updating js file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-alert("Hello World!")
-let weight = document.getElementById("in").value;  // Get the value from the input
-weight = Number(weight); // Convert it to a number
-if (weight > 15) {
-    alert("Baggage exceeds the 15kg allowance!");
-}
-else {
-    alert("Baggage is within the allowed limit.");
+alert("This is an inline script!")
+let c = () => {
+    let i = document.getElementById("in").value;
+    let weight = parseFloat(i);
+
+    if (weight > 15) {
+        alert("Baggage exceeds the 15kg allowance!");
+    } 
+    else {
+        alert("Baggage is within the allowed limit.");
+    }
 }
