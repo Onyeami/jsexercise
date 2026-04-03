094fc59f78043105e659821de96502920ff934bd
Onyeami <134443934+Onyeami@users.noreply.github.com>
2026-04-03 20:26:02 +0100

    function2

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ let c = () => {
     }
 }
 let doIt = () => alert("Not Implemented");
+
+function doIt() {
+    let sum = 0;
+
+    for (let i = 0; i < 1000; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+    alert("The sum is: " + sum);
+}
